023138cf851234e40690288c4aa542e265f82ced
Samuele <46517478+SamueleLonghin@users.noreply.github.com>
2025-06-22 10:22:05 +0200

    creo il file se db non esiste

--- db/database.py
+++ db/database.py
@@ -10,13 +10,15 @@ SCHEMA_PATH = os.path.join(os.path.dirname(__file__), "schema.sql")
 
 def get_db():
     def adapt_empty_string(value):
-        return None if value == '' else value
+        return None if value == "" else value
 
     sqlite3.register_adapter(str, adapt_empty_string)
 
     def convert_date(val):
         try:
-            return datetime.strptime(val.decode(), current_app.config["DB_DATE_FORMAT"]).date()
+            return datetime.strptime(
+                val.decode(), current_app.config["DB_DATE_FORMAT"]
+            ).date()
         except Exception:
             return val
 
@@ -29,9 +31,13 @@ def get_db():
 
 def init_db():
     if not os.path.exists(DB_PATH):
+        print("Database non trovato, lo creo...")
+        os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
         print("Inizializzo database...")
         conn = get_db()
         with open(SCHEMA_PATH, "r") as f:
             conn.executescript(f.read())
         conn.commit()
         conn.close()
+    else:
+        print("Database già esistente, non è necessario inizializzarlo.", DB_PATH)
