a87d0601ce04a82a545739cfd04b8e2fd27c17a8
Samuele <46517478+SamueleLonghin@users.noreply.github.com>
2025-06-22 10:07:34 +0200

    spostato il db per permettere il volume

--- db/database.py
+++ db/database.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 from flask import current_app
 
-DB_PATH = os.path.join(os.path.dirname(__file__), "database.db")
+DB_PATH = os.path.join(os.path.dirname(__file__), "data/database.db")
 SCHEMA_PATH = os.path.join(os.path.dirname(__file__), "schema.sql")
 
 
